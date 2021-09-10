c239ba71ab220f8588d72f84450937015668a17b
tothzalan <toth.zalan03@gmail.com>
2021-09-10 10:28:52 +0200

    Dynamics

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
+class Upgrade {
+    constructor(name, price, description) {
+        this._name = name
+        this._price = price
+        this._description = description
+        this._owned = 0
+    }
+    get name() {
+        return this._name
+    }
+    set name(name) {
+        this._name = name
+    }
+    get price() {
+        return this._price
+    }
+    set price(price) {
+        this._price = price
+    }
+    get description() {
+        return this._description
+    }
+    get owned() {
+        return this._owned
+    }
+    set owned(owned) {
+        this._owned = owned
+    }
+}
+
+let upgrades = [new Upgrade("finger", 15, "autoclicks"), new Upgrade("grandma", 100, "bakes cookies"),
+new Upgrade("factory", 1000, "produces cookies in large quantities"), new Upgrade("alchemy", 50000, "turns gold into cookies")]
+
+
 let cookies = 0
-let cps = 0 
+let cps = 0
 
-let fingerPrice = 15
-let fingersOwned = 0
-
-let grandmaPrice = 100
-let grandmasOwned = 0
-
-let factoryPrice = 1000
-let factoriesOwned = 0
-
-let alchemyPrice = 50000
-let alchemyOwned = 0
-
-setInterval(() => { 
+setInterval(() => {
     cookies += cps
     changeText()
 }, 1000)
 
 let cookieImage = false;
 const getCookie = () => {
-    if(cookieImage)
+    if (cookieImage)
         document.getElementById("cookieImage").src = "img/cookie.png"
     else
         document.getElementById("cookieImage").src = "img/cookie2.png"
     cookieImage = !cookieImage
-    cookies += 1 
+    cookies += 1
     changeText()
 }
 
@@ -36,61 +58,63 @@ const changeText = () => {
     numberText.innerHTML = `you have ${cookies.toFixed(2)} cookies`
     cPsText.innerHTML = `cookies / second: ${cps.toFixed(2)}`
 
-    if(cookies >= fingerPrice)
-        document.getElementById("fingerUpgrade").style = "background-color: gray"
-    if(cookies >= grandmaPrice)
-        document.getElementById("grandmaUpgrade").style = "background-color: gray"
-    if(cookies >= factoryPrice)
-        document.getElementById("factoryUpgrade").style = "background-color: gray"
-    if(cookies >= alchemyPrice)
-        document.getElementById("alchemyUpgrade").style = "background-color: gray"
+    upgrades.forEach(obj => {
+        if(cookies >= obj.price)
+            document.getElementById(`${obj.name}Upgrade`).style = "background-color: gray"
+    })
 }
 
-const buyFinger = () => {
-    if(cookies >= fingerPrice) {
-        fingersOwned += 1
-        cookies -= fingerPrice
-        fingerPrice = Math.ceil(fingerPrice * 1.5)
-        cps += 0.2
-        changeText()
-        document.getElementById("fingerPrice").innerHTML = `price: ${fingerPrice}`
-        document.getElementById("fingerOwned").innerHTML = `owned: ${fingersOwned}`
-        document.getElementById("fingerUpgrade").style = "background-color: rgb(90,90,90)"
-    }
-} 
-const buyGrandma = () => {
-    if(cookies >= grandmaPrice) {
-        grandmasOwned += 1
-        cookies -= grandmaPrice
-        grandmaPrice = Math.ceil(grandmaPrice * 2)
-        cps += 1
-        changeText()
-        document.getElementById("grandmaPrice").innerHTML = `price: ${grandmaPrice}`
-        document.getElementById("grandmaOwned").innerHTML = `owned: ${grandmasOwned}`
-        document.getElementById("grandmaUpgrade").style = "background-color: rgb(90,90,90)"
+
+
+const buyAction = (obj) => {
+    if (cookies > obj.price) {
+        cookies -= obj.price
+        obj.price = Math.ceil(obj.price * 2.5)
+        obj.owned += 1
+        cps += obj.price / 10
+        document.getElementById(`${obj.name}Owned`).innerHTML = `owned: ${obj.owned}`
+        document.getElementById(`${obj.name}Price`).innerHTML = `price: ${obj.price}`
+        document.getElementById(`${obj.name}Upgrade`).style = "background-color: rgb(90,90,90)"
     }
 }
-const buyFactory = () => {
-    if(cookies >= factoryPrice) {
-        factoriesOwned += 1
-        cookies -= factoryPrice 
-        factoryPrice = Math.ceil(factoryPrice * 1.5)
-        cps += 10
-        changeText()
-        document.getElementById("factoryPrice").innerHTML = `price: ${factoryPrice}`
-        document.getElementById("factoryOwned").innerHTML = `owned: ${factoriesOwned}`
-        document.getElementById("factoryUpgrade").style = "background-color: rgb(90,90,90)"
+
+const createUpgrades = (obj) => {
+    let div = document.createElement("div")
+    div.classList.add("upgradeItem")
+    div.id = `${obj.name}Upgrade`
+
+    let nameParagraph = document.createElement("p")
+    nameParagraph.innerHTML = obj.name
+    div.appendChild(nameParagraph)
+
+    let priceParagraph = document.createElement("p")
+    priceParagraph.innerHTML = `price: ${obj.price}`
+    priceParagraph.id = `${obj.name}Price`
+    div.appendChild(priceParagraph)
+
+    let descParagraph = document.createElement("p")
+    descParagraph.innerHTML = obj.description
+    div.appendChild(descParagraph)
+
+    let ownedParagraph = document.createElement("p")
+    ownedParagraph.id = `${obj.name}Owned`
+    ownedParagraph.innerHTML = "owned: 0"
+    div.appendChild(ownedParagraph)
+
+    let buyButton = document.createElement("button")
+    buyButton.innerHTML = "Buy"
+    buyButton.onclick = function () {
+        buyAction(obj)
+        return false
     }
-} 
-const buyAlchemy = () => {
-    if(cookies >= alchemyPrice) {
-        alchemyOwned += 1
-        cookies -= alchemyPrice 
-        alchemyPrice = Math.ceil(alchemyPrice * 2)
-        cps += 100
-        changeText()
-        document.getElementById("alchemyPrice").innerHTML = `price: ${alchemyPrice}`
-        document.getElementById("alchemyOwned").innerHTML = `owned: ${alchemyOwned}`
-        document.getElementById("alchemyUpgrade").style = "background-color: rgb(90,90,90)"
-    }
+    div.appendChild(buyButton)
+
+    document.getElementsByClassName("upgradeBar")[0].appendChild(div)
+}
+
+
+window.onload = () => {
+    upgrades.forEach(element => {
+        createUpgrades(element)
+    })
 }
